a4efb54ac7ad4418e3702698e66cdd31f1b54f5b
jackzhangpython <jackzhang.python@gmail.com>
2016-07-10 23:24:41 +0800

    Add - 开始按钮

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,43 @@
 
+var StartLayer = cc.Layer.extend({
+    ctor: function(){
+        this._super();
+
+        var startItem = new cc.MenuItemImage(
+            resource.start_N,
+            resource.start_S,
+            function(){
+                cc.log("menu is clicked!")
+            },
+            this
+        );
+        startItem.attr({
+            x: cc.winSize.width / 2,
+            y: cc.winSize.height / 2,
+            anchorX : 0.5,
+            anchorY : 0.5
+        });
+
+        var menu = new cc.Menu(startItem);
+        menu.x = 0;
+        menu.y = 0;
+        this.addChild(menu, 1);
+    }
+});
 
 var PokerGame = cc.Scene.extend({
     onEnter: function(){
         this._super();
-        var startSprite = cc.Sprite.create(resource.start);
-        startSprite.setPosition(cc.director.getWinSize().width / 2, cc.director.getWinSize().height / 2);
-        startSprite.setScale(0.8);
-        this.addChild(startSprite, 0);
+        // var startSprite = cc.Sprite.create(resource.start);
+        // startSprite.setPosition(cc.director.getWinSize().width / 2, cc.director.getWinSize().height / 2);
+        // startSprite.setScale(0.8);
+        // this.addChild(startSprite, 0);
+
+        var startLayer = new StartLayer();
+        this.addChild(startLayer);
     }
 });
+
 
 window.onload = function(){
     cc.game.onStart = function(){
--- src/js/resource.js
+++ src/js/resource.js
@@ -1,3 +1,5 @@
 var resource = {
-    start :"src/images/Start.png"
+    start :"src/images/Start.png",
+    start_N :"src/images/start_N.png",
+    start_S :"src/images/start_S.png"
 };
